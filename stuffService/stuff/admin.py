1853c62e601538e0d7145e39c05c9c28d69a0a63
nihither <semenov.an.sh@gmail.com>
2025-05-05 21:49:36 +0300

    add models + 0001 migration

--- stuffService/stuff/admin.py
+++ stuffService/stuff/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from stuff.models import Teacher, Student, Lesson, Message
+
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+
+
+class MessageInline(admin.StackedInline):
+    model = Message
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone']
+    inlines = [LessonInline]
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'parent_first_name', 'parent_last_name']
+    inlines = [LessonInline, MessageInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['teacher', 'student', 'day']
+
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Message)
--- stuffService/stuff/models.py
+++ stuffService/stuff/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
-# Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(verbose_name="Имя", max_length=40, blank=False, null=False)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=30, blank=True, null=True)
+    birthday = models.DateField(verbose_name="День рождения", blank=True, null=True)
+    phone = models.CharField(verbose_name="Телефон", max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Student(models.Model):
+    first_name = models.CharField(verbose_name="Имя", max_length=40, blank=False, null=False)
+    last_name = models.CharField(verbose_name="Фамилия", max_length=30, blank=True, null=True)
+    birthday = models.DateField(verbose_name="День рождения", blank=True, null=True)
+    phone = models.CharField(verbose_name="Телефон", max_length=15, blank=True, null=True)
+    has_parent = models.BooleanField(verbose_name="Есть контактное лицо", default=True)
+    parent_first_name = models.CharField(verbose_name="Имя родителя", max_length=40, blank=True, null=True)
+    parent_last_name = models.CharField(verbose_name="Фамилия родителя", max_length=30, blank=True, null=True)
+    parent_birthday = models.DateField(verbose_name="День рождения родителя", blank=True, null=True)
+    parent_phone = models.CharField(verbose_name="Телефон родителя", max_length=15, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super(Student, self).save(*args, **kwargs)
+            new_messages = Message()
+            new_messages.student = self
+            new_messages.save()
+
+
+class Lesson(models.Model):
+    WEEKDAYS = (
+        (0, "ПН"),
+        (1, "ВТ"),
+        (2, "СР"),
+        (3, "ЧТ"),
+        (4, "ПТ"),
+        (5, "СБ"),
+        (6, "ВС")
+    )
+    teacher = models.ForeignKey(to=Teacher, on_delete=models.SET_NULL, blank=True, null=True,
+                                related_name="lessons", verbose_name="Преподаватель")
+    student = models.ForeignKey(to=Student, on_delete=models.SET_NULL, blank=True, null=True,
+                                related_name="lessons", verbose_name="Ученик")
+    day = models.SmallIntegerField(verbose_name="День недели", choices=WEEKDAYS)
+    timestamp = models.TimeField(verbose_name="Время")
+
+    def __str__(self):
+        return f"{self.teacher} {self.student} {self.day}"
+
+
+class Message(models.Model):
+    reminder_is_required = models.BooleanField(verbose_name="Нужно ли напоминать?", default=True)
+    reminder_message = models.TextField(verbose_name="Текст для напоминания",
+                                        default="Добрый день.\r\n"
+                                                "Напоминаем об уроке завтра dd.mm в hh:MM")
+    report_is_required = models.BooleanField(verbose_name="Нужно ли сообщать о проведении?", default=True)
+    report_message = models.TextField(verbose_name="Текст для отчета",
+                                      default="""Добрый день.\r\nУрок провели.\r\nОсталось i оплаченных занятий из n""")
+    student = models.OneToOneField(to=Student, on_delete=models.CASCADE, related_name="messages", blank=False,
+                                   null=False)
+
+    def __str__(self):
+        return f"Шаблоны сообщений для {self.student}"
